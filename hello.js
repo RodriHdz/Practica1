4c401ce8f9a736608fc4a377355f799aec16ca21
Rodrigo Quiroz <rodrigoqhdz11@outlook.com>
2026-09-11 13:13:56 -0600

    Primer commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+//console.log("Hello world");
+
+//console.log("Rodrigo Quiroz")
+//console.log("JavaScript")
+//console.log("Quiero crear una app de registro de pagos")
+let nombre = "Rodrigo";
+console.log("hello world"+nombre);
+
