5e806a0de9ef92ff9e5bb3a8bb374c019c67d6fa
Rodrigo Quiroz <rodrigoqhdz11@outlook.com>
2026-09-14 13:34:27 -0600

    agregue contador

---
+++ contador.py
@@ -0,0 +1,11 @@
+for i in range(1, 11):
+    print("El numero es ", i)
+
+    if i < 5:
+        print("- Numero pequeño")
+
+    elif i == 5:
+        print("- Llegamos a 5")
+
+    else:
+        print("- Numero grande")
